adf047746e17fc1472abbf01fb19b771157686f9
masaris888 <malfarizipratama62@gmail.com>
2025-06-16 22:09:24 +0700

    membuat footer

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,19 +1,15 @@
- <!-- component footer -->
- <template>
+<!-- membuat component footer -->
+<template>
   <footer class="bg-gray-100 text-center p-4 mt-10">
     <p>&copy; 2025 KostKu. All rights reserved.</p>
   </footer>
 </template>
 <!-- end component footer -->
 
-
 <script>
 export default {
-    name: 'FooterView',
-
-}
+  name: "FooterView",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
